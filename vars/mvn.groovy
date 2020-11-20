89a4be04f2fd6f377cfcf43dfada57806761544b
Matthias Cullmann <matthias.cullmann@baloise.com>
2020-11-20 12:46:24 +0100

    FIX signature

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -2,6 +2,6 @@ import com.baloise.sharedlib.common.Registry
 import com.baloise.sharedlib.demo.Greeting
 import com.baloise.sharedlib.tools.Maven
 
-def call(String cmd, String mavenVersion = Maven.MAVEN_VERSION_DEFAULT, String jdkVersion = "nope") {
-	new Registry(this).getService(Maven.class).mvn(cmd, mavenVersion, jdkVersion)
+def call(String cmd, String tag = "") {
+	new Registry(this).getService(Maven.class).mvn(cmd, tag)
 }
