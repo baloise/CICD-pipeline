cc54e42075d9f5ed62985a02ab621be3657396bb
Matthias Cullmann <matthias.cullmann@baloise.com>
2020-11-23 16:40:13 +0100

    make hack explicit

--- src/com/baloise/sharedlib/tools/internal/MavenImpl.groovy
+++ src/com/baloise/sharedlib/tools/internal/MavenImpl.groovy
@@ -1,7 +1,10 @@
 package com.baloise.sharedlib.tools.internal
 
+import static com.baloise.sharedlib.common.FakeItUntilYouMakeIt.fake
+
 import java.nio.file.*
 
+import com.baloise.sharedlib.common.FakeItUntilYouMakeIt
 import com.baloise.sharedlib.common.Registered
 import com.baloise.sharedlib.tools.Maven
 
@@ -11,17 +14,17 @@ class MavenImpl extends Registered implements Maven{
 	// tag see https://hub.docker.com/_/maven
 	public void mvn(String cmd, String tag = "") {
 		String image = tag ? "maven:$tag" : "maven"
-		if(steps.isUnix()) {
-			steps.docker.image(image).inside('-v $HOME/.m2:/root/.m2') {
-                steps.sh "mvn $cmd"
-			}
-		} else {
+		if(fake(this).isWindows()) {
 			String sname = "docker-settings.xml"
 			String settings = 
-				Files.exists(Paths.get(System.getProperty("user.home"), '.m2').toRealPath().resolve(sname)) ? 
+			Files.exists(Paths.get(System.getProperty("user.home"), '.m2').toRealPath().resolve(sname)) ? 
 				"-s /root/.m2/$sname" : 
 				""
-			steps.bat "docker run --rm -v \"%cd%\":/usr/src/mymaven -v \"%userprofile%\\.m2\":/root/.m2 -w /usr/src/mymaven $image mvn $settings $cmd"
+				steps.bat "docker run --rm -v \"%cd%\":/usr/src/mymaven -v \"%userprofile%\\.m2\":/root/.m2 -w /usr/src/mymaven $image mvn $settings $cmd"
+		} else {
+			steps.docker.image(image).inside('-v $HOME/.m2:/root/.m2') {
+				steps.sh "mvn $cmd"
+			}
 		}
 	}
 }
