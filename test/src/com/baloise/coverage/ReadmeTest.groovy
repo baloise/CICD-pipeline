b623855656df22264fd5fd4d430d071d61be90b8
Matthias Cullmann <matthias.cullmann@baloise.com>
2020-12-04 17:56:55 +0100

    handle forward slash in branch name

--- test/src/com/baloise/coverage/ReadmeTest.groovy
+++ test/src/com/baloise/coverage/ReadmeTest.groovy
@@ -11,7 +11,7 @@ class ReadmeTest {
 	void branchNameMustBeInCoverageLink() {
 		File HEAD = new File(".git/HEAD")
 		if(!HEAD.exists()) return
-		String branch = new File(".git/HEAD").text.split('/')[-1].trim()
+		String branch = new File(".git/HEAD").text[16..-1].trim()
 		int c = 3
 		assertEquals(c, new File("README.md").text.count("/CICD-pipeline/site/$branch/"), "occurences of /CICD-pipeline/site/$branch/") 
 	}
