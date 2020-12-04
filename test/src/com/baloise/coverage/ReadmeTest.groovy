b122595dcb0402830ab69963546a6cd840ecdb70
Matthias Cullmann <matthias.cullmann@baloise.com>
2020-12-04 17:32:43 +0100

    robustness

--- test/src/com/baloise/coverage/ReadmeTest.groovy
+++ test/src/com/baloise/coverage/ReadmeTest.groovy
@@ -9,6 +9,8 @@ class ReadmeTest {
 	
 	@Test
 	void branchNameMustBeInCoverageLink() {
+		File HEAD = new File(".git/HEAD")
+		if(!HEAD.exists()) return
 		String branch = new File(".git/HEAD").text.split('/')[-1].trim()
 		int c = 3
 		assertEquals(c, new File("README.md").text.count("/CICD-pipeline/site/$branch/"), "occurences of /CICD-pipeline/site/$branch/") 
